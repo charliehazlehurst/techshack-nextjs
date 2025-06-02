5fb4a1bfcc1c892d150183d14b9c8fdcb0dfad1e
Charlie Hazlehurst <charliehazlehurst31@gmail.com>
2025-06-02 21:33:57 +0100

    Changed default export in supabase.js

--- app/auth/callback/page.js
+++ app/auth/callback/page.js
@@ -3,7 +3,7 @@
 
 import { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { supabase } from '/lib/supabase.js';
+import supabase from '/lib/supabase.js';
 
 export default function CallbackPage() {
   const router = useRouter();
