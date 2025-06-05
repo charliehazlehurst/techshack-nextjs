bd607f1a030ae980fb34fcc010102d292a2e051f
Charlie Hazlehurst <charliehazlehurst31@gmail.com>
2025-06-05 12:20:28 +0100

    Reverted supabase.ts & added supabaseAdmin.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,8 @@
-// lib/supabaseAdmin.ts
+// /lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // this must be the service role key
-);
+const supabaseUrl = 'https://faomnoqjqwksuskjstum.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhb21ub3FqcXdrc3Vza2pzdHVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNzM0ODgsImV4cCI6MjA2MjY0OTQ4OH0.L2NwtsveWv11Bw8WzdbJecY6eL1uh5YAcFRD0TDgcF8';
 
-export default supabaseAdmin;
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
